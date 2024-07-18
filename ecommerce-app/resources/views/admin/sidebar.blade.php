5ddbb44c5fda59ab62f12cb2e9f9efabd1ced9a1
valium69mg <carlostranquilino.cr@gmail.com>
2024-07-18 17:26:03 -0600

    Add a nav-item for the user to see the store orders

--- ecommerce-app/resources/views/admin/sidebar.blade.php
+++ ecommerce-app/resources/views/admin/sidebar.blade.php
@@ -88,6 +88,14 @@
               <span class="menu-title">Category</span>
             </a>
           </li>
+          <li class="nav-item menu-items">
+            <a class="nav-link" href="{{url('storeOrders')}}">
+              <span class="menu-icon">
+                <i class="mdi mdi-playlist-play"></i>
+              </span>
+              <span class="menu-title">Store Orders </span>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- partial -->
