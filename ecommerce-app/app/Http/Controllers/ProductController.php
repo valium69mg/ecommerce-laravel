e9edb05f07f5c6c77e2a349af8e68abd9252e37a
valium69mg <carlostranquilino.cr@gmail.com>
2024-07-26 16:38:00 -0600

    Refactor code un product controller

--- ecommerce-app/app/Http/Controllers/ProductController.php
+++ ecommerce-app/app/Http/Controllers/ProductController.php
@@ -10,82 +10,90 @@ use File;
 class ProductController extends Controller
 {
     public function addProduct(Request $request){
-        $product = new Product();
-        $product->title = $request->title;
-        $product->description = $request->description;
-        $product->price = $request->price;
-        $product->quantity = $request->quantity;
-        $product->category = $request->category;
-        $product->discount_price = $request->discount_price;
-        $image = $request->image;
-        $imageName = time().'.'. $image->getClientOriginalExtension();
-        $request->image->move('product', $imageName);
-        $product->img_name = $imageName;
-        $product->save();
-        return redirect()->back()->with("message","Product created succesfully");
-    }
-
-    public function showProducts(Request $request) {
-        if (Auth::user()) {
-            $userType = Auth::user()->usertype;
-            if ($userType === 1) { // ADMIN
-                $data = Product::all();
-                return view('admin.product-view',compact('data'));
-            } else if ($userType === 0) { // NOT ADMIN
-                return view('dashboard');
-            }
-        } else {
-            return view('welcome');
-        }
-    }
-
-    public function editProduct($id) {
-        
-        if (Auth::user()) {
-            $userType = Auth::user()->usertype;
-            if ($userType === 1) { // ADMIN
-                $data = Category::all();
-                $product = Product::find($id);
-                return view('admin.edit-product',compact('product','data'));
-            } else if ($userType === 0) { // NOT ADMIN
-                return view('dashboard');
-            }
-        } else {
-            return view('welcome');
-        }
-    }
-
-    public function updateProduct(Request $request,$id) {
-        $product = Product::find($id);
-        $product->title = $request->title;
-        $product->description = $request->description;
-        $product->price = $request->price;
-        $product->quantity = $request->quantity;
-        $product->category = $request->category;
-        $product->discount_price = $request->discount_price;
-        if ($request->hasFile('image')) {
-            if (File::exists(public_path('product/'. $product->img_name))) {
-                File::delete(public_path('product/'. $product->img_name));
-            }
+        $user = Auth::user();
+        if ($user->usertype === 1) { // IF USER IS ADMIN
+            $product = new Product();
+            $product->title = $request->title;
+            $product->description = $request->description;
+            $product->price = $request->price;
+            $product->quantity = $request->quantity;
+            $product->category = $request->category;
+            $product->discount_price = $request->discount_price;
             $image = $request->image;
             $imageName = time().'.'. $image->getClientOriginalExtension();
             $request->image->move('product', $imageName);
             $product->img_name = $imageName;
+            $product->save();
+            return redirect()->back()->with("message","Product created succesfully");
         }
-        $product->save();
-        return redirect()->back()->with("message","Product edited succesfully");
+        // NOT ADMIN
+        return redirect()->route('home');
+    }
+
+    public function showProducts(Request $request) {
+        $userType = Auth::user()->usertype;
+        if ($userType === 1) { // ADMIN
+            $data = Product::all();
+            return view('admin.product-view',compact('data'));
+        } 
+        // NOT ADMIN
+        return redirect()->route('home');
+        
+    }
+
+    public function editProduct($id) {
+        $userType = Auth::user()->usertype;
+        if ($userType === 1) { // ADMIN
+            $data = Category::all();
+            $product = Product::find($id);
+            return view('admin.edit-product',compact('product','data'));
+        }
+        // NOT ADMIN
+        return redirect()->route('home');
+    }
+
+    public function updateProduct(Request $request,$id) { 
+        if (Auth::user()->usertype === 1) {
+            $product = Product::find($id);
+            $product->title = $request->title;
+            $product->description = $request->description;
+            $product->price = $request->price;
+            $product->quantity = $request->quantity;
+            $product->category = $request->category;
+            $product->discount_price = $request->discount_price;
+            if ($request->hasFile('image')) {
+                if (File::exists(public_path('product/'. $product->img_name))) {
+                    File::delete(public_path('product/'. $product->img_name));
+                }
+                $image = $request->image;
+                $imageName = time().'.'. $image->getClientOriginalExtension();
+                $request->image->move('product', $imageName);
+                $product->img_name = $imageName;
+            }
+            $product->save();
+            return redirect()->back()->with("message","Product edited succesfully");
+        }
+        // NOT ADMIN
+        return redirect()->route('home');
     }
 
     public function deleteProduct($id) {
-        $product = Product::find($id);
-        $product->delete();
-        return redirect()->back()->with("message","Product deleted succesfully");
+        if (Auth::user()->usertype === 1) {
+            $product = Product::find($id);
+            $product->delete();
+            return redirect()->back()->with("message","Product deleted succesfully");
+        }
+        // NOT ADMIN
+        return redirect()->route('home');
     }
 
-    public function getDetails($id) {
-        $product = Product::find($id);
-        return view("products.product-details", compact("product"));
-        
+    public function getDetails($id) { 
+        if (Auth::user()->usertype === 1) {
+            $product = Product::find($id);
+            return view("products.product-details", compact("product"));
+        }
+        // NOT ADMIN
+        return redirect()->route('home');
     }
 
 }
